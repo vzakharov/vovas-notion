467b34ff3a43da8817e3c02e5b41b12ec0a6238f
Vova Zakharov <vzakharov@gmail.com>
2023-02-20 12:23:20 +0100

    to ts

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,97 @@
 // Various methods to work with Notion API
 
-console.log("Starting vovas-notion version 1.1.0")
+import axios, { AxiosInstance } from 'axios'
+import * as _ from 'lodash'
+import * as pluralize from 'pluralize'
 
-// ES6:
-// import axios from 'axios'
-// import _ from 'lodash'
-// import pluralize from 'pluralize'
+const { chain, camelCase, keys } = _
 
-// CommonJS:
-const axios = require('axios')
-const _ = require('lodash')
-const pluralize = require('pluralize')
+export interface NotionOptions {
+  debug?: boolean
+  baseURL?: string
+  ignoreTokenWarning?: boolean
+}
 
-const { 
-  chain, camelCase, upperFirst, keys
-} = _
+export interface IDatabase {
+  database_id: string
+}
+
+type Content = any;
+type Data = Record<string, any>;
+
+// function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION_API_URL, debug = false } = {} ) {
+export default class Notion {
 
 
-function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION_API_URL, debug = false } = {} ) {
+  // console.log({ debug, token, baseURL })
 
+  // let api = axios.create({
+  //   baseURL,
+  //   headers: {
+  //     ...token ? {
+  //       Authorization: `Bearer ${token}`,
+  //       'Notion-Version': '2022-02-22'
+  //     } : {}
+  //   }
+  // })
 
-  console.log({ debug, token, baseURL })
+  // console.log( 'API:', api )
 
-  let api = axios.create({
-    baseURL,
-    headers: {
-      ...token ? {
-        Authorization: `Bearer ${token}`,
-        'Notion-Version': '2022-02-22'
-      } : {}
+  token?: string
+  api: AxiosInstance
+  static anon: Notion = new Notion()
+
+  constructor(token?: string, options: NotionOptions = {} ) {
+
+    let { debug, baseURL, ignoreTokenWarning } = options
+
+    if ( !token ) {
+      console.warn('No token provided. The API won\'t work unless you create your own server/proxy that has the same interface as Notion API and accepts non-authenticated requests.')
+    } else {
+      // If this is a browser environment, throw an error because it is insecure to expose the token to the client
+      if ( typeof window !== 'undefined' ) {
+        if ( !ignoreTokenWarning ) {
+          throw new Error('Notion API token should not be exposed to the client. Please either move all API calls to the server or use a proxy that accepts non-authenticated requests. If you are sure you want to do this, pass `ignoreTokenWarning: true` to the constructor.')
+        }
+      }
     }
-  })
 
-  console.log( 'API:', api )
+    debug ??= false
+    baseURL ??= process.env.NOTION_API_URL ?? 'https://api.notion.com/v1/'
+    this.api = axios.create({
+      baseURL,
+      headers: {
+        ...token ? {
+          Authorization: `Bearer ${token}`,
+          'Notion-Version': '2022-02-22'
+        } : {}
+      }
+    })
+
+    this.token = token
+
+    if ( debug ) {
+      console.log( 'Notion instance created:', this )
+    }
+
+  }
   
-  Object.assign(this, {
+  // Object.assign(this, {
 
     // Create page
-    async createPage({ parent, properties, titleProp, content }) {
+    // async createPage({ parent, properties, titleProp, content }) {
+    async createPage(options: {
+      parent: IDatabase,
+      properties: Data,
+      titleProp?: string,
+      content?: Content
+    }): Promise<Data> {
+
+      let { parent, properties, titleProp, content } = options
 
       let {
         data
-      } = await api.post('pages', {
+      } = await this.api.post('pages', {
         parent,
         ...notionize({ properties, content, titleProp })
       })
@@ -50,14 +100,20 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
 
       return data
 
-    },
+    }
 
     // Update page
-    async updatePage(id, { properties, content }) {
+    // async updatePage(id, { properties, content }) {
+    async updatePage(id: string, options: {
+      properties: Data,
+      content?: Content
+    }): Promise<Data> {
+
+      let { properties, content } = options
 
       let {
         data
-      } = await api.patch(`pages/${id}`,
+      } = await this.api.patch(`pages/${id}`,
         notionize({ properties, content })
       )
 
@@ -65,23 +121,25 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
 
       return data
 
-    },
+    }
 
     // Delete block
-    async deleteBlock(id) {
+    // async deleteBlock(id) {
+    async deleteBlock(id: string): Promise<any> {
 
-      let { data } = await api.delete(`blocks/${id}`)
+      let { data } = await this.api.delete(`blocks/${id}`)
 
       return data
 
-    },
+    }
 
     // Get page
-    async getPage(id) {
+    // async getPage(id) {
+    async getPage(id: string): Promise<Data> {
 
       let {
         data
-      } = await api.get(`pages/${id}`)
+      } = await this.api.get(`pages/${id}`)
 
       data = denotionize(data)
 
@@ -89,26 +147,33 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
 
       return data
 
-    },
+    }
 
     // Get token owner info
-    async getUser() {
+    // async getUser() {
+    async getUser(): Promise<any> {
 
       // If no token, throw error
-      if ( !token ) {
+      if ( !this.token ) {
         throw new Error('No token provided. Please use an instance of Notion with a token.')
       }
 
       return (
-        await api.get('users/me')
+        await this.api.get('users/me')
       ).data
 
-    },
+    }
 
     // query a database
-    async queryDatabase(databaseId, query, { unwrap: unwrapKeys } = {} ) {
+    // async queryDatabase(databaseId, query, { unwrap: unwrapKeys } = {} ) {
+    async queryDatabase(databaseId: string, query: any, options: {
+      unwrap?: string[]
+    } = {} ): Promise<Data[]> {
+
+      let { unwrap: unwrapKeys } = options
+
       let data = (
-        await api.post(`databases/${databaseId}/query`, query)
+        await this.api.post(`databases/${databaseId}/query`, query)
       ).data.results.map(denotionize)
 
       // console.log(`Database ${databaseId} queried:`, data)
@@ -119,10 +184,11 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
 
       return data
 
-    },
+    }
 
     // Get page by name
-    async getPageBy(databaseId, filter ) {
+    // async getPageBy(databaseId, filter ) {
+    async getPageBy(databaseId: string, filter: Record<string, string | number>): Promise<Data> {
       let key = keys(filter)[0]
       let value = filter[key]
       return ( 
@@ -135,12 +201,17 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
           }
         })
       )?.[0]
-    },
+    }
 
     // get a block
-    async getBlock(blockId, { recurse = false } = {}) {
+    // async getBlock(blockId, { recurse = false } = {}) {
+    async getBlock(blockId: string, options: {
+      recurse?: boolean
+    } = {}): Promise<any> {
 
-      let { data, data: { has_children } } = await api.get(`blocks/${blockId}`)
+      let { recurse = false } = options
+
+      let { data, data: { has_children } } = await this.api.get(`blocks/${blockId}`)
 
       if ( has_children ) {
         data.children = await this.getBlockChildren(blockId, { recurse })
@@ -148,16 +219,21 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
 
       return data
 
-    },
+    }
 
     // get block children
-    async getBlockChildren(blockId, { recurse = false } = {}) {
+    // async getBlockChildren(blockId, { recurse = false } = {}) {
+    async getBlockChildren(blockId: string, options: {
+      recurse?: boolean
+    } = {}): Promise<any[]> {
 
-      let { data: { results }} = await api.get(`blocks/${blockId}/children`)
+      let { recurse = false } = options
+
+      let { data: { results }} = await this.api.get(`blocks/${blockId}/children`)
 
       if ( recurse )
         await Promise.all(
-          results.map( async result => {
+          results.map( async (result: any) => {
             let { has_children } = result
             if ( has_children ) {
               result.children = await this.getBlockChildren(result.id, { recurse })
@@ -168,18 +244,19 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
       
       return results
 
-    },
+    }
 
-    async unwrap(data, unwrapKeys) {
+    // async unwrap(data, unwrapKeys) {
+    async unwrap(data: Data[], unwrapKeys: string[]): Promise<void[]> {
       // Fetches complete page data for pages that only contain the id
 
-      let cache = {}
-      let promises = []
+      let cache: Record<string, Promise<Data>> = {}
+      let promises: any[] = []
     
       // For all keys that represent a relation, get the related page
       for (let result of data) {
     
-        for (let key in unwrapKeys) {
+        for (let key of unwrapKeys) {
           let items = result[key]
 
           // If it's not an array, make it one
@@ -188,10 +265,10 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
 
           // console.log('Items:', items)
               
-          promises.push(Promise.all(items.map(async (item) => {
+          promises.push(Promise.all(items.map(async (item: any) => {
     
             // console.log('item:', item)
-            let { id } = item
+            let { id }: { id: string } = item
 
             if ( !id ) {
               console.log('No id found for item:', item)
@@ -220,13 +297,20 @@ function Notion(token = process.env.NOTION_TOKEN, { baseURL = process.env.NOTION
     }
         
 
-  })
+  // })
 
 }
 
-function notionize({ properties, titleProp = 'name', content }) {
+// function notionize({ properties, titleProp = 'name', content }) {
+export function notionize(config: {
+  properties: Data,
+  titleProp?: string,
+  content?: Content
+}): any {
 
-  let jsonKeys = []
+  let { properties, titleProp = 'name', content } = config
+
+  let jsonKeys: string[] = []
 
   return {
 
@@ -300,7 +384,7 @@ function notionize({ properties, titleProp = 'name', content }) {
         content = (
           typeof content.plain === 'string' ? content.plain : JSON.stringify(content.plain, null, 2)
         ).split(/\n+/),
-        content.map( line => ({
+        content.map( (line: string) => ({
           type: 'paragraph',
           paragraph: {
             rich_text: [{
@@ -317,14 +401,20 @@ function notionize({ properties, titleProp = 'name', content }) {
   }
 }
 
-function denotionize(data, { propKey = 'properties', unwrap } = {} ) {
+// function denotionize(data, { propKey = 'properties', unwrap } = {} ) {
+export function denotionize(data: Data, options: {
+  propKey?: string,
+  unwrap?: string[]
+} = {}): any {
 
-  let jsonKeys = []
+  let { propKey = 'properties', unwrap } = options
+
+  let jsonKeys: string[] = []
 
   data = {
     raw: data,
     ...chain(data[propKey])
-      .mapKeys( ( value, key ) => {
+      .mapKeys( ( value, key: string ) => {
         key = camelCase(key)
         if ( key.endsWith('Json') ) {
           let type = 'object'
@@ -335,7 +425,7 @@ function denotionize(data, { propKey = 'properties', unwrap } = {} ) {
       })
       .mapValues( ( object, key ) => {
 
-        const extract = object =>
+        const extract = (object: any): any =>
           object?.type ?
             object.type == 'select' ?
               object.select?.name
@@ -370,14 +460,6 @@ function denotionize(data, { propKey = 'properties', unwrap } = {} ) {
 
 }
 
+// Notion.anon = new Notion()
 
-Notion.anon = new Notion()
-
-console.log('vovas-notion loaded')
-
-// ES6
-// export default Notion
-// CommonJS
-module.exports = {
-  default: Notion,
-}
+// console.log('vovas-notion loaded')
